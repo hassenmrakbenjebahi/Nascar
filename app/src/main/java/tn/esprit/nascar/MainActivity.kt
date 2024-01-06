8e4897d42666dfe9108937e3898ea9379696328a
hassenmrakbenjebahi <123837615+hassenmrakbenjebahi@users.noreply.github.com>
2024-01-06 23:33:27 +0100

    recycleView

--- app/src/main/java/tn/esprit/nascar/MainActivity.kt
+++ app/src/main/java/tn/esprit/nascar/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import tn.esprit.nascar.databinding.ActivityMainBinding
+import tn.esprit.nascar.fragment.AboutFragment
+import tn.esprit.nascar.fragment.EventsFragment
+import tn.esprit.nascar.fragment.NewsFragment
+import tn.esprit.nascar.fragment.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +23,9 @@ class MainActivity : AppCompatActivity() {
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //TODO 11 Bind the toolbar to the activity
         val toolbar: Toolbar = binding.toolbar.appBar
         setSupportActionBar(toolbar)
-        //TODO 1 Implement the click on the 3 buttons (btnNews|btnEvents|btnProfile) to call changeFragment(...)
+
         binding.btnNews.setOnClickListener {
             changeFragment(NewsFragment(), "")
         }
@@ -35,28 +38,23 @@ class MainActivity : AppCompatActivity() {
             changeFragment(ProfileFragment(), "")
         }
 
-        //TODO 2 Implement the first call of the first fragment
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, NewsFragment()).commit()
-
     }
 
     private fun changeFragment(fragment: Fragment, name: String) {
 
-        //TODO 3 Check if name is empty then replace the new fragment in the activity.
-        //    If tag is not empty replace the new fragment in the activity and use addToBackStack
         if (name.isEmpty())
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
         else
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).addToBackStack("").commit()
+
     }
 
-    //TODO 12 Override the method onCreateOptionsMenu()
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
-    //TODO 13 Override the method onOptionsItemSelected() and Implement info and logout behavior
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         when(item.itemId){
--- app/src/main/java/tn/esprit/nascar/fragment/AboutFragment.kt
+++ app/src/main/java/tn/esprit/nascar/fragment/AboutFragment.kt
@@ -1,15 +1,16 @@
-package tn.esprit.nascar
+package tn.esprit.nascar.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tn.esprit.nascar.R
 
-//TODO 5 Change this class to a fragment and use binding to inflate fragment_events.xml
+class AboutFragment: Fragment() {
 
-class EventsFragment:Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_events, container, false)
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
+
 }
